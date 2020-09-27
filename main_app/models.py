6ab2c5f751f038fb8c6198a3c2c2acc13832b1cc
Alice Huh <aklee213@gmail.com>
2020-09-27 00:29:33 -0400

    [models.py] Add Post model

---
+++ main_app/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    TOY = 'TY'
+    DIAPER = 'DP'
+    CLOTHES = 'CL'
+    BOOK = 'BK'
+    OTHERS = 'OT'
+    THREEMONTH = '3M'
+    SIXMONTH = '6M'
+    NINEMONTH = '9M'
+    ONEYEAR = '1y'
+    TWOYEAR = '2Y'
+    THREEYEAR = '3Y'
+    FOURYEAR = '4Y'
+    FIVEYEAR = '5Y'
+    NEW = 'NW'
+    LIKEANEW = 'LN'
+    GOOD = "GD"
+    FAIR = "FR"
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=300)
+    CATEGORY_CHOICES = [
+        (TOY, 'Toys'),
+        (DIAPER, 'Diapers'),
+        (CLOTHES, 'Clothes'),
+        (BOOK, 'Books'),
+        (OTHERS, 'Others'),
+    ]
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORY_CHOICES,
+        default=OTHERS,
+    )
+    AGE_CHOICES = [
+        (THREEMONTH,'3m'),
+        (SIXMONTH, '6m'),
+        (NINEMONTH, '9m'),
+        (ONEYEAR, '1yr'),
+        (TWOYEAR, '2yr'),
+        (THREEYEAR, '3yr'),
+        (FOURYEAR, '4yr'),
+        (FIVEYEAR, '5yr'),
+    ]
+    age = models.CharField(
+        max_length=2,
+        choices=AGE_CHOICES,
+        default=THREEMONTH,
+    )
+    CONDITION_CHOICES = [
+        (NEW, 'New'),
+        (LIKEANEW, 'Like a new'),
+        (GOOD, 'Good'),
+        (FAIR, 'Fair'),
+    ]
+    condition = models.CharField(
+        max_length=2,
+        choices=CONDITION_CHOICES,
+    )
+    price = models.IntegerField
+    sold = models.BooleanField(default=True)
+    img = models.ImageField(upload_to='images/')
+    
+
+    
+    
+
+
+    def __str__(self):
+        return self.title
---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Hello, World</h1>')
+
+def post_index(request):
+
+    posts = Post.objects.all()
+    return render(request, 'posts/index.html', {'posts:posts'})
